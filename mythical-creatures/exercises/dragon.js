2a384729fb93849c2e96452b80fea6fbc91631cd
Allison Dietz <dietz.allison@outlook.com>
2021-05-18 17:53:54 -0600

    Pass Dragon unit tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,23 @@
 
 class Dragon {
-  constructor() {
-
+  constructor(name, rider) {
+    this.name = name,
+    this.rider = rider,
+    this.hungry = true,
+    this.timesFed = 0
   }
+
+  greet() {
+    return 'Hi, ' + this.rider + '!'
+  }
+
+  eat() {
+    this.timesFed ++
+    if (this.timesFed >=3) {
+      this.hungry = false
+    }
+  }
+
 }
 
 module.exports = Dragon;
