44a3e252bafc800a6e1b53e2869ead9eeccf00f8
Allison Dietz <dietz.allison@outlook.com>
2021-05-21 20:07:52 -0600

    Pass Pirate unit tests

--- mythical-creatures/exercises/pirate.js
+++ mythical-creatures/exercises/pirate.js
@@ -4,22 +4,39 @@ class Pirate{
     this.job = job,
     this.cursed = false,
     this.booty = 0,
+    this.robberies = 0
   }
+
+
+  // outer function state: how many times / if it's been called
+  // 
 
   robShip() {
-    counter += 1
-
-    console.log('COUNTER >>>>> ', counter)
-
-    if (counter < 5) {
-      this.booty += 100
-      return 'YAARRR!';
-    } else if (counter >= 5) {
-      this.cursed = true
-      return 'ARG! I\'ve been cursed!';
-    }
+      if (this.robberies < 5) {
+console.log('COUNTER >>>>> ', this.robberies)
+        this.robberies ++
+        this.booty += 100
+console.log('SUCCESSFUL ROBBERY! >>>>> ', this.robberies)
+            return 'YAARRR!';
+      } else if (this.robberies >= 5) {
+console.log('COUNTER >>>>> ', this.robberies)
+        this.robberies ++
+        this.cursed = true
+console.log('CURSED! >>>>> ', this.robberies)
+        return 'ARG! I\'ve been cursed!';
+      }
   }
 
+  liftCurse() {
+    if (this.cursed) {
+      this.booty -= 300
+      this.cursed = false
+      return 'Your curse has been lifted!';
+    } else if (!this.cursed) {
+      return 'You don\'t need to lift a curse!';
+    }
+  }
+  
 }
 
 module.exports = Pirate;
