c76961a9a0457209aa9069e6016cc2057eb21d25
Allison Dietz <dietz.allison@outlook.com>
2021-05-22 22:21:10 -0600

    Pass Medusa unit tests

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -6,6 +6,21 @@ class Medusa{
     this.name = name,
     this.statues = statues || []
   }
+
+  gazeAtVictim(victim) {
+    if (this.statues.length < 3) {
+      let stoned = new Statue(victim.name)
+      this.statues.push(stoned)
+      return stoned;
+    } else if (this.statues.length >= 3) {
+      let freeVictim = this.statues.shift()
+      let restoredToPerson = new Person(freeVictim.name, 'relieved')
+      let stoned = new Statue(victim.name)
+      this.statues.push(stoned)
+      return restoredToPerson;
+    }
+  }
+
 }
 
 module.exports = Medusa;
