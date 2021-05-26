8887721aa4a7d8a673a7a27bdf8b02c2e451ac07
Allison Dietz <dietz.allison@outlook.com>
2021-05-26 11:25:28 -0600

    Update/refactor Medusa tests and implementation

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -8,14 +8,13 @@ class Medusa{
   }
 
   gazeAtVictim(victim) {
+    let stoned = new Statue(victim.name)
     if (this.statues.length < 3) {
-      let stoned = new Statue(victim.name)
       this.statues.push(stoned)
       return stoned;
     } else if (this.statues.length >= 3) {
       let freeVictim = this.statues.shift()
       let restoredToPerson = new Person(freeVictim.name, 'relieved')
-      let stoned = new Statue(victim.name)
       this.statues.push(stoned)
       return restoredToPerson;
     }
